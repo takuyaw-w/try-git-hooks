4deb83d80ca2986f9869300218e2744659a3ad28
takuyaw-w <takuya.jamtb@gmail.com>
2023-07-30 10:45:43 +0900

    test commit2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <h1>hello</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
